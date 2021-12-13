a6a4404abde405d46580c2609941aa6342199a3e
sergio-gonzalez-sainz <s3rgio.sainz@gmail.com>
2021-12-12 21:12:44 -0600

    Detalles De Objetos

--- 01-fundamentos/assets/js/objeto-literal.js
+++ 01-fundamentos/assets/js/objeto-literal.js
@@ -7,7 +7,13 @@
     se imprimen por orden alfabetico.
  */
 
-let personaje = {
+/*  Al inicializar un objeto como const lo que no estamos 
+    permitiendo es que se pueda mutar el "personajes", es decir, 
+    inicializar "personje" como un string, un entero, una contante
+    un booleano, un arreglo, etc.
+    Pero Sí permite modificar o agregar propiedades al objeto. 
+ */
+const personaje = {
     nombre: 'Tony Stark',
     codeName: 'Ironman',
     vivo: false,
@@ -45,6 +51,38 @@ const x = 'vivo';
 
 console.log('Vivo:', personaje[x] );
 console.log('Ultima Película:', personaje['ultima_pelicula']);
+
+//Más detalles
+
+/*  Para borrar una propieda de un objeto se ocupa 
+    la palabra reservada delete.
+*/
+delete personaje.edad ;
+console.log(personaje);
+
+personaje.casado = true;
+
+/*  entries: Permite ver el objeto como arreglos
+*/
+const entriPares = Object.entries(personaje);
+console.log( entriPares );
+
+console.log(personaje);
+
+/*  Para hacer un bloqueo de la modificación de las propiedades
+    existentes del objeto.Pero no a los objeto dentro de los objetos.
+    Se utiliza freeze.
+*/
+Object.freeze(personaje);
+
+personaje.direccion.ubicacion = 'Costa Rica';
+console.log(personaje)
+
+const propiedades = Object.getOwnPropertyNames(personaje);
+const valores = Object.values(personaje);
+console.log(propiedades,valores);
+
+
 
 
 
